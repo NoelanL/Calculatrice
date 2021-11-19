e4fe34a0b192aa9c038905b55cddab656d017327
NoelanL <92019519+NoelanL@users.noreply.github.com>
2021-11-19 14:51:53 +0100

    Finalisation de la calculatrice.

---
+++ app.js
@@ -0,0 +1,23 @@
+function button(value){
+    document.getElementById('screen').innerHTML += value;
+}
+function buttonReset(){
+    document.getElementById('screen').innerHTML = "";
+}
+function buttonAddition(){
+    document.getElementById('screen').innerHTML += "+";
+}
+function buttonSoustraction(){
+    document.getElementById('screen').innerHTML += "-";
+}
+function  buttonMultiplication(){
+    document.getElementById('screen').innerHTML += "*";
+}
+function  buttonModulo(){
+    document.getElementById('screen').innerHTML += "/";
+}
+function buttonEgale(){
+    document.getElementById('screen').innerHTML = eval(document.getElementById('screen').innerHTML);
+}
+
+
